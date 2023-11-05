d194e0c9a59b8a533a2755987ecfae8394935525
J/ <55988027+jkerai1@users.noreply.github.com>
2023-11-05 20:46:27 +0000

    Update RemoveDuplicates.py

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,19 +1,16 @@
-import re
+import re                                                  #Tool Written by Jkerai1 https://github.com/jkerai1
 def remove_duplicate():
     # opens txt in r mode as one long string and assigns to var
     records  = open('CLSID list.txt', 'r').read()
     # .split() removes excess whitespaces from str, return str as list
     records = re.split("\s{2,}", records)
-    # empty list to store non-duplicate e-mails
-    clean_list = []
-    # for loop to append non-duplicate records to clean list
-    for record in records:
+    clean_list = []                                   # empty list to store non-duplicate e-mails
+    
+    for record in records:                          # for loop to append non-duplicate records to clean list
         if record not in clean_list:
             clean_list.append(record)
     return clean_list
-    # close txt file
     records.close()
-# assigns CLSID_no_duplicate_records.txt to variable below
 CLSID_no_duplicate_records = open('CLSID_no_duplicate_records.txt', 'w')
 
 # function to convert clean_list 'list' elements in to strings
@@ -23,6 +20,5 @@ for record in remove_duplicate():
     CLSID_no_duplicate_records.write(f"{record}\n")
 # close CLSID_no_duplicate_records.txt file
 CLSID_no_duplicate_records.close()
-
 
 remove_duplicate() ## Duplicates refer to CLSIDs and not by application!
