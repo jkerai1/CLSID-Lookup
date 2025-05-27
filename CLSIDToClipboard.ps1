3a169eb94dc0057b3488d06ed3fe4e5eafd3cc00
J/ <55988027+jkerai1@users.noreply.github.com>
2025-05-27 20:32:38 +0000

    Update CLSIDToClipboard.ps1

--- CLSIDToClipboard.ps1
+++ CLSIDToClipboard.ps1
@@ -1,2 +1,2 @@
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Classes\CLSID*" |select PSChildName,(default`) |ft -auto * | clip
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Classes\CLSID\*"|select PSChildName,`(default`) |ft -auto *| clip
 # This could be turned into a cmdlet for repeated use
